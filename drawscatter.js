32177b955aaf3c818ce0b8cc8098484000f21ba9
FFS01 <roylevisay28@gmail.com>
2022-11-29 23:50:49 -0800

    change height and width

--- drawscatter.js
+++ drawscatter.js
@@ -24,8 +24,8 @@ function callcontin(id, url, givenTopic, givenclass, givenfeature) {
 
     // set the dimensions and margins of the graph
     const margin = {top: 10, right: 30, bottom: 30, left: 40},
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        width = 230 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     const svg = d3.select(id)
